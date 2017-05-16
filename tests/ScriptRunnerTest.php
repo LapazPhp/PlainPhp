f1957a0933d85c8fa848c4b5dad4f818c25dc3c3
Hisateru Tanaka <tanakahisateru@gmail.com>
2017-05-16 15:27:23 +0900

    Add context branching test

--- tests/ScriptRunnerTest.php
+++ tests/ScriptRunnerTest.php
@@ -49,6 +49,26 @@ class ScriptRunnerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals([1, 2], $result);
     }
 
+    public function testDoRequireWithDifferentContext()
+    {
+        $prototypeRunner = ScriptRunner::create()->with(['foo' => 1]);
+
+        $runner1 = $prototypeRunner->with(['bar' => 1]);
+        $runner2 = $prototypeRunner->with(['bar' => 2]);
+        $runner3 = $runner2->binding($this);
+
+        $this->assertNotSame($prototypeRunner, $runner1);
+        $this->assertNotSame($prototypeRunner, $runner2);
+        $this->assertNotSame($prototypeRunner, $runner3);
+        $this->assertNotSame($runner1, $runner2);
+        $this->assertNotSame($runner1, $runner3);
+        $this->assertNotSame($runner2, $runner3);
+
+        $this->assertEquals([1, 1], $runner1->doRequire(__DIR__ . '/scripts/return-foo-bar.php'));
+        $this->assertEquals([1, 2], $runner2->doRequire(__DIR__ . '/scripts/return-foo-bar.php'));
+        $this->assertSame($this, $runner3->doRequire(__DIR__ . '/scripts/return-this.php'));
+    }
+
     public function testDoRequireWithBoundObject()
     {
         $result = ScriptRunner::create()->binding($this)->doRequire(__DIR__ . '/scripts/return-this.php');
